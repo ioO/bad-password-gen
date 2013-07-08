42cea8eb954768813bd48ee32d1fc2491f56c17c
Lionel <lionel@soreia.com>
2013-07-08 22:54:27 +0200

    remove readme info in source file

--- bpwgen.py
+++ bpwgen.py
@@ -1,22 +1,4 @@
 #!/bin/env python
-
-# This program generates a numeric password from the length you pass.
-# This is a bad idea to use numeric password but sometimes some website doesn't
-# allow you another solution.
-# So I dedicated this licence to this kind of website :
-
-#               DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-#                       Version 2, December 2004
-
-# Everyone is permitted to copy and distribute verbatim or modified
-# copies of this license document, and changing it is allowed as long
-# as the name is changed.
-
-#               DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-#       TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-
-# 0. You just DO WHAT THE FUCK YOU WANT TO.
-
 import argparse, random
 
 parser = argparse.ArgumentParser(description='Generate numeric password')
